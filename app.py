51b4c0ee47f7b4336f81c215b8484ae90e26af94
costi-tica <costi13tica@gmail.com>
2021-12-12 17:12:50 +0200

    added db

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 
 from flask import Flask
-from flask_mqtt import Mqtt
+# from flask_mqtt import Mqtt
 from threading import Thread
 import json
 import time
+import db
 import src.models.status as status
 from src.example_endpoint import example_endpoint
 
+app = None
 
-app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = 'localhost'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = ''
-app.config['MQTT_PASSWORD'] = ''
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
+def start_app():
+    global app
+    app = Flask(__name__)
+    app.config['MQTT_BROKER_URL'] = 'localhost'
+    app.config['MQTT_BROKER_PORT'] = 1883
+    app.config['MQTT_USERNAME'] = ''
+    app.config['MQTT_PASSWORD'] = ''
+    app.config['MQTT_KEEPALIVE'] = 5
+    app.config['MQTT_TLS_ENABLED'] = False
 
-# register blueprints
-app.register_blueprint(example_endpoint)
+    # register blueprints
+    app.register_blueprint(example_endpoint)
+    #mqtt = Mqtt(app)
 
-mqtt = Mqtt(app)
+    db.init_app(app)
+
+    return app
     
 # fucntion for mqtt's thread    
 def background_thread():
     while True:
         time.sleep(1)
         message = json.dumps(status.get_status(), default=str)
-        mqtt.publish('smart_bed', message)
+        #mqtt.publish('smart_bed', message)
 
 def init_mqtt_thread():
     thread = Thread(target=background_thread)
@@ -35,5 +42,6 @@ def init_mqtt_thread():
 
 
 if __name__ == '__main__':
-    init_mqtt_thread()
+    app = start_app()
+    #init_mqtt_thread()
     app.run()
